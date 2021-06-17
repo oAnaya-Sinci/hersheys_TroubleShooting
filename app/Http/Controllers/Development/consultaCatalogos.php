6dd6c7f3b3b01afbe4f06c404787eb48d034bdae
Omar Anaya <oanaya@sinci.com>
2021-06-17 12:23:09 -0500

    Cambio a la consulta de catalogos

--- app/Http/Controllers/Development/consultaCatalogos.php
+++ app/Http/Controllers/Development/consultaCatalogos.php
@@ -24,72 +24,6 @@ class consultaCatalogos extends Controller
     }
 
     /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create()
-    {
-        //
-    }
-
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
-    public function store(Request $request)
-    {
-        //
-    }
-
-    /**
-     * Display the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function show($id)
-    {
-        //
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function edit($id)
-    {
-        //
-    }
-
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function update(Request $request, $id)
-    {
-        //
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function destroy($id)
-    {
-        //
-    }
-
-    /**
      *
     */
 
@@ -101,16 +35,20 @@ class consultaCatalogos extends Controller
                         ->leftJoin('catalogos AS grand_grand_parent', 'grand_parent.ctg_padre', '=', 'grand_grand_parent.ctg_id')
                         ->join('jerarquia_catalogos AS jc', 'catalogos.ctg_tipo', '=', 'jc.jrq_id')
                         ->where('catalogos.ctg_eliminado', 0)
-                        ->distinct()->get();
+                        ->where('parent.ctg_eliminado', 0)
+                        ->where('grand_parent.ctg_eliminado', 0)
+                        ->where('grand_grand_parent.ctg_eliminado', 0)
+                        ->distinct()
+                        ->get();
 
         return $Elementos;
     }
 
     public function deleteCatalog(Request $data){
 
-        // catalogos::where('id', $data['data'])->update(['ctg_eliminado' => '1']);
+        catalogos::where('id', $data['data'])->update(['ctg_eliminado' => '1']);
 
-        catalogos::where('id', $data['data'])->delete();
+        // catalogos::where('id', $data['data'])->delete();
 
         return true;
     }
